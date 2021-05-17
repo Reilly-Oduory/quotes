54b115f2e80e340abaa184231cd0b4c79f4f475b
Reilly-Oduory <reilly.oduory@student.moringaschool.com>
2021-05-17 17:26:55 +0300

    styling

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -31,6 +31,13 @@ export class QuoteComponent implements OnInit {
     this.quotes.push(quote)
   }
 
+  quoteDelete(index:number){
+    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
+    if(toDelete){
+      this.quotes.splice(index,1)
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
